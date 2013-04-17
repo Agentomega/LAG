07f2b29e8bdf30a26551369a2473079a8a14e1b6
allenj12 <allenj12@rpi.edu>
2013-04-17 03:47:12 -0400

    getConclusion now generates random conclusion succesfully however a size limit might need to be implemented

--- main/main.hs
+++ main/main.hs
@@ -1,9 +1,15 @@
+-- Logical Argument Generator
+-- Author: Joe Allen && Joe Valerio
+-- Computability and Logic: Final project
+
+
 {-# LANGUAGE UnicodeSyntax #-}
 import System.Random(randomRIO)
 import Data.Random.Extras(choice)
 import Data.Random.RVar(runRVar)
 import Data.Random
 import Data.Char (digitToInt)
+import Data.List(elemIndices, splitAt )
 
 ----------------------------------------------------------------------------------------------------prompting user
 getArguments :: IO Int
@@ -12,11 +18,13 @@ getArguments =
        arguments <- getChar
        return (digitToInt arguments)
 
+
 getValidity :: IO Bool
 getValidity =
     do putStrLn "Please enter \n(1)for valid\n(2) for invalid"
        prevalidity <- getChar
        return (digitToInt prevalidity == 1)
+
 
 getRange :: IO (Int,Int)
 getRange = 
@@ -28,6 +36,7 @@ getRange =
         preupper <- getChar
         let upper = digitToInt preupper
         return (lower,upper)
+
 
 getOperators :: IO [Int]
 getOperators = 
@@ -58,9 +67,23 @@ checkAtoms :: String -> String -> Bool
 checkAtoms atom1 atom2 = 
    not (atom1 /= "$" && atom2 /= "$") || (atom1 /= atom2)
 
+
+expandDol :: String -> String -> [String] -> IO String
+expandDol conclusion operators atoms = do
+    let (ys,zs) = splitAt (head (take 1 replacement)) conclusion
+    conclusion' <- getConclusion operators atoms
+    if length replacement > 1 then expandDol (ys ++ conclusion' ++ tail zs) operators atoms
+     else return (ys ++ conclusion' ++ tail zs)
+  where replacement = elemIndices '$' conclusion
+
+
+
 getConclusion :: String -> [String] -> IO String
-getConclusion operators atoms =
-   runRVar (choice [atom1 ++ " " ++ [operator] ++ " " ++ atom2 | atom1 <- atoms, atom2 <- atoms, operator <- operators,checkAtoms atom1 atom2]) StdRandom
+getConclusion operators atoms = do
+   conclusion <- runRVar (choice ["(" ++ atom1 ++ " " ++ [operator] ++ " " ++ atom2 ++ ")"| atom1 <- atoms, atom2 <- atoms, operator <- operators, checkAtoms atom1 atom2]) StdRandom
+   if '$' `elem` conclusion then expandDol conclusion operators atoms
+    else return conclusion
+
 
 makePremise :: IO String -> String -> Int -> [String] -> IO String
 makePremise = undefined
@@ -90,5 +113,6 @@ main =
     atomRange <- getRange
     dummyOperators <- getOperators
     let operators = map convertOperators dummyOperators
+    makeArgument operators premRange atomRange
     putStrLn "Thank You!\nExecuting..."
     putStrLn "Good Bye"
